d10e345bdd6368e67ea7b9a3dbe3db417818b6f4
Danilo Pianini <danilo.pianini@unibo.it>
2021-10-05 18:47:08 +0200

    Enable the build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
 rootProject.name = "multi-jvm-test-plugin"
 enableFeaturePreview("VERSION_CATALOGS")
+
+plugins {
+    id("com.gradle.enterprise") version "3.6.3"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
