eb850b47d89f3910f96b8801c460871a56ddd26d
Danilo Pianini <danilo.pianini@unibo.it>
2023-02-17 18:01:25 +0100

    build: limit testing under Windows GHA runners

--- src/test/resources/org/danilopianini/multijvmtesting/test/define-custom-configuration/build.gradle.kts
+++ src/test/resources/org/danilopianini/multijvmtesting/test/define-custom-configuration/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.apache.tools.ant.taskdefs.condition.Os
+
 plugins {
     id("org.danilopianini.multi-jvm-test-plugin")
 }
@@ -21,5 +23,18 @@ multiJvm {
     println(latestJava)
     println(allLtsVersions)
     println(supportedJvmVersions.get())
-    testByDefaultWith(8, *allLtsVersions.toIntArray(), latestJava)
+    if (System.getenv("GITHUB_ACTIONS") == "true" && Os.isFamily(Os.FAMILY_WINDOWS)) {
+        // There is limited space available on GitHub Actions Windows instances:
+        // only test the most recent version of Java there.
+        logger.warn(
+            "Detected a GitHub Actions Windows runner. Window runners have very limited disk space," +
+                    "and thus all tests will run solely with Java {}.",
+            latestJavaSupportedByGradle,
+        )
+        testByDefaultWith(latestJavaSupportedByGradle)
+    } else {
+        testByDefaultWith(8, *allLtsVersions.toIntArray(), latestJava)
+    }
 }
+
+
--- src/test/resources/org/danilopianini/multijvmtesting/test/simple-java-build/build.gradle.kts
+++ src/test/resources/org/danilopianini/multijvmtesting/test/simple-java-build/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.apache.tools.ant.taskdefs.condition.Os
+
 plugins {
     id("org.danilopianini.multi-jvm-test-plugin")
 }
@@ -13,4 +15,17 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+if (System.getenv("GITHUB_ACTIONS") == "true" && Os.isFamily(Os.FAMILY_WINDOWS)) {
+    // There is limited space available on GitHub Actions Windows instances:
+    // only test the most recent version of Java there.
+    multiJvm {
+        logger.warn(
+            "Detected a GitHub Actions Windows runner. Window runners have very limited disk space," +
+                    "and thus all tests will run solely with Java {}.",
+            latestJavaSupportedByGradle,
+        )
+        testByDefaultWith(latestJavaSupportedByGradle)
+    }
 }
