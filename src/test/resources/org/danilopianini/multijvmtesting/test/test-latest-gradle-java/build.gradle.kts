ee100986dafb8ba4209a4aef8727a908e1147c8e
Danilo Pianini <danilo.pianini@unibo.it>
2023-04-21 17:48:59 +0200

    test: add a test to try that the latest gradle version can be intercepted correctly

---
+++ src/test/resources/org/danilopianini/multijvmtesting/test/test-latest-gradle-java/build.gradle.kts
@@ -0,0 +1,23 @@
+import org.apache.tools.ant.taskdefs.condition.Os
+
+plugins {
+    id("org.danilopianini.multi-jvm-test-plugin")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+multiJvm {
+    jvmVersionForCompilation.set(8)
+    maximumSupportedJvmVersion.set(latestJavaSupportedByGradle)
+}
