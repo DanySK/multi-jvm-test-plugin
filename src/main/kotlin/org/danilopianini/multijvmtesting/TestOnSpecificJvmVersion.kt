f797c08de751c1a9d598b518b734527074a11fd1
Danilo Pianini <danilo.pianini@unibo.it>
2024-07-15 19:00:02 +0200

    fix: work around `Class 'TestOnSpecificJvmVersion' is not abstract and does not implement abstract base class member 'getDryRun'.`. See also: https://discuss.gradle.org/t/custom-task-and-dryrun-in-gradle-8x/47882/2

--- src/main/kotlin/org/danilopianini/multijvmtesting/TestOnSpecificJvmVersion.kt
+++ src/main/kotlin/org/danilopianini/multijvmtesting/TestOnSpecificJvmVersion.kt
@@ -1,15 +1,20 @@
 package org.danilopianini.multijvmtesting
 
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.testing.Test
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JavaToolchainService
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.property
 import javax.inject.Inject
 
 /**
  * A special [Test] task configured to run the tests using a specific JVM Version.
  */
 open class TestOnSpecificJvmVersion @Inject constructor(jvmVersion: Int) : Test() {
+
+    private lateinit var isADryRun: Property<Boolean>
+
     init {
         group = TASK_GROUP
         description = makeTaskDescription(jvmVersion)
@@ -27,5 +32,14 @@ open class TestOnSpecificJvmVersion @Inject constructor(jvmVersion: Int) : Test(
 
         internal fun makeTaskDescription(version: Int) =
             "Runs the unit tests using a Java Virtual Machine (JVM) version $version."
+    }
+
+    @Suppress("UnstableApiUsage")
+    override fun getDryRun(): Property<Boolean> = when {
+        ::isADryRun.isInitialized -> isADryRun
+        else -> {
+            isADryRun = project.objects.property<Boolean>()
+            isADryRun
+        }
     }
 }
