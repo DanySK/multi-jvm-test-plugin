0753b60135685d1af806ec37133f4106bdf737e5
Danilo Pianini <danilo.pianini@unibo.it>
2024-07-15 19:05:54 +0200

    build: exclude detekt from the Kotlin version enforcement

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
 }
 
 // Enforce Kotlin version coherence
-configurations.all {
+configurations.matching { it.name != "detekt" }.all {
     resolutionStrategy.eachDependency {
         if (requested.group == "org.jetbrains.kotlin" && requested.name.startsWith("kotlin")) {
             useVersion(KOTLIN_VERSION)
