d43952b2b8a0ee8e3ebd50fef3ec84594c0bbc9a
Danilo Pianini <danilo.pianini@unibo.it>
2024-07-18 18:16:22 +0200

    refactor: leave `TestOnSpecificJvmVersion` abstract instead of using horrible kludges (thanks @Vampire for the suggestion)

--- src/main/kotlin/org/danilopianini/multijvmtesting/TestOnSpecificJvmVersion.kt
+++ src/main/kotlin/org/danilopianini/multijvmtesting/TestOnSpecificJvmVersion.kt
@@ -1,19 +1,15 @@
 package org.danilopianini.multijvmtesting
 
-import org.gradle.api.provider.Property
 import org.gradle.api.tasks.testing.Test
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JavaToolchainService
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.property
 import javax.inject.Inject
 
 /**
  * A special [Test] task configured to run the tests using a specific JVM Version.
  */
-open class TestOnSpecificJvmVersion @Inject constructor(jvmVersion: Int) : Test() {
-
-    private lateinit var isADryRun: Property<Boolean>
+abstract class TestOnSpecificJvmVersion @Inject constructor(jvmVersion: Int) : Test() {
 
     init {
         group = TASK_GROUP
@@ -38,14 +34,5 @@ open class TestOnSpecificJvmVersion @Inject constructor(jvmVersion: Int) : Test(
 
         internal fun makeTaskDescription(version: Int) =
             "Runs the unit tests using a Java Virtual Machine (JVM) version $version."
-    }
-
-    @Suppress("UnstableApiUsage")
-    override fun getDryRun(): Property<Boolean> = when {
-        ::isADryRun.isInitialized -> isADryRun
-        else -> {
-            isADryRun = project.objects.property<Boolean>()
-            isADryRun
-        }
     }
 }
