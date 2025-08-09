5a7a1ad69fc2b0e10acfdaa7ba2c1b2d7292e9fb
Danilo Pianini <danilo.pianini@unibo.it>
2025-08-09 02:36:46 +0200

    feat: add full support for kotlin multiplatform projects (#954)

--- src/main/kotlin/org/danilopianini/multijvmtesting/TestOnSpecificJvmVersion.kt
+++ src/main/kotlin/org/danilopianini/multijvmtesting/TestOnSpecificJvmVersion.kt
@@ -2,50 +2,64 @@ package org.danilopianini.multijvmtesting
 
 import javax.inject.Inject
 import org.gradle.api.file.FileCollection
+import org.gradle.api.internal.tasks.testing.TestFramework
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JavaToolchainService
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 /**
- * A special [Test] task configured to run the tests using a specific [jvmVersion].
+ * A special [Test] task configured to run the [referenceTest] using a specific [jvmVersion].
  */
 abstract class TestOnSpecificJvmVersion
 @Inject
-constructor(@Internal val jvmVersion: Int) : Test() {
+constructor(
+    @Internal val jvmVersion: Int,
+    @Internal val referenceTest: Test,
+) : Test() {
+
     init {
         group = TASK_GROUP
         description = makeTaskDescription(jvmVersion)
-        val launcher =
-            project
-                .provider { project.extensions.getByType<JavaToolchainService>() }
-                .flatMap { toolchains ->
-                    val launcher =
-                        toolchains.launcherFor {
-                            it.languageVersion.set(JavaLanguageVersion.of(jvmVersion))
-                        }
-                    runCatching { launcher.isPresent }
-                        .onFailure {
-                            enabled = false
-                            project.logger.warn(
-                                "Although declared as supported in the multiJvm configuration, " +
-                                    "no Java {} distribution is available for the current operating system. " +
-                                    "task {} will be disabled.",
-                                jvmVersion,
-                                name,
-                            )
-                        }
-                    launcher
-                }
+        val launcher = project
+            .provider { project.extensions.getByType<JavaToolchainService>() }
+            .flatMap { toolchains ->
+                val launcher =
+                    toolchains.launcherFor {
+                        it.languageVersion.set(JavaLanguageVersion.of(jvmVersion))
+                    }
+                runCatching { launcher.isPresent }
+                    .onFailure {
+                        enabled = false
+                        project.logger.warn(
+                            "Although declared as supported in the multiJvm configuration, " +
+                                "no Java {} distribution is available for the current operating system. " +
+                                "task {} will be disabled.",
+                            jvmVersion,
+                            name,
+                        )
+                    }
+                launcher
+            }
         javaLauncher.set(launcher)
+        dependsOn(project.tasks.withType<KotlinCompilationTask<*>>())
+        dependsOn(project.tasks.withType<JavaCompile>())
     }
 
-    override fun getTestClassesDirs(): FileCollection = super.getTestClassesDirs()
-        ?: project.layout.buildDirectory
-            .dir("classes/kotlin/jvm/test")
-            .get()
-            .asFileTree
+    override fun getClasspath(): FileCollection = referenceTest.classpath
+
+    override fun getTestClassesDirs(): FileCollection = referenceTest.testClassesDirs
+
+    override fun getTestFramework(): TestFramework = referenceTest.testFramework
+
+    override fun getTestFrameworkProperty(): Property<TestFramework> = referenceTest.testFrameworkProperty
+
+    override fun isScanForTestClasses(): Boolean = referenceTest.isScanForTestClasses
 
     internal companion object {
         internal const val TASK_GROUP = "Verification"
