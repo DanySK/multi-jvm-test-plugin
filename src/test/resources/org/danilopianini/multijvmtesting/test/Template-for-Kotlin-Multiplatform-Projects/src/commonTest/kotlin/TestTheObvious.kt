627d76a4caf87bd9cb083a6422dbdea8077860a9
Danilo Pianini <danilo.pianini@unibo.it>
2025-08-14 01:37:37 +0200

    fix: work around missing explicit dependencies among tasks in the Kotlin Gradle Plugin (#964)

---
+++ src/test/resources/org/danilopianini/multijvmtesting/test/Template-for-Kotlin-Multiplatform-Projects/src/commonTest/kotlin/TestTheObvious.kt
@@ -0,0 +1,9 @@
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class TestTheObvious {
+    @Test
+    fun `test that this test is run`() {
+        assertTrue(true, "This test should always pass")
+    }
+}
